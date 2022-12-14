707c68ff74e12cacb4e71e10ac04e600fa086ea3
Stanislav Minko <minkostas@gmail.com>
2022-12-14 03:47:07 +0300

    feat: string_1 init

---
+++ string_1/8/app.js
@@ -0,0 +1,30 @@
+/*
+Пользователь вводит номер месяц. Необходимо вывести пору года по номеру
+месяца (2 способа)
+
+ */
+
+const m = +prompt("введите # месяца");
+
+if (m === 12 || m === 1 || m === 3) {
+  console.log("зима");
+} else if ( 2< m && m < 6) {
+  console.log("весна");
+} else if ( 5< m && m < 9) {
+  console.log("лето");
+} else if ( 8< m && m < 12) {
+  console.log("осень");
+} else {
+    console.log(`некоректный ввод`);
+}
+
+switch (m) {
+    case value:
+    case value:
+    case value:
+
+        break;
+
+    default:
+        break;
+}
