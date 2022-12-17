6bcaa6c0ddd07523777f66f42e83223477bb019a
Stanislav Minko <minkostas@gmail.com>
2022-12-17 05:09:10 +0300

    feat: string_2_6

--- string_2/7/app.js
+++ string_2/7/app.js
@@ -1,16 +1,16 @@
 /*
- На вход программе подаётся строка.
- Замените все @ на '!' с помощью глобального
+На вход программе подаётся строка. Замените все @ на '!' с помощью глобального
 поиска и замены (2 способа)
-
 */
 
-let str = prompt('enter string').trim();
+let str = prompt('enter string');
 
 if(isNaN(str)){
     console.log(str.split(`@`).join(' '));
     console.log(str.replaceAll(`@`, `!`));
-
 } else {
     console.log(`error`);
 }
+
+let str1 = prompt('enter another string');
+
