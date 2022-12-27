67e08b205878d8d63420462c6234ce8c4fef6310
Stanislav Minko <minkostas@gmail.com>
2022-12-28 00:05:05 +0300

    feat: cycles_1_12_(nah)

--- 15-cycles_1/12/app.js
+++ 15-cycles_1/12/app.js
@@ -18,19 +18,23 @@ console.log(
   "mom",
   "dad"
 );
+let poly = 0;
 
 if (isNaN(a)) {
   a = a.split("");
   for (let i = 0, j = a.length - 1; i < a.length, j !== -1; i++, j--) {
     if (a[i] === a[j]) {
       console.log(a[i], "=", a[j]);
+      poly++;
     } else {
-      console.log("не палиндром");
-      break;
+      poly--;
     }
   }
+  if (poly > 0) {
+    console.log(`слово '${a.join("")}' - палиндром`);
+  } else {
+    console.log("не палиндром");
+  }
 } else {
-    console.log("ввели НЕ слово");
+  console.log("ввели НЕ слово");
 }
-console.log(`слово '${a.join("")}' - палиндром`);
-
