b3887f41731bef82882370363b22482cede2c406
Stanislav Minko <minkostas@gmail.com>
2023-01-04 01:55:16 +0300

    feat: 19-array-method_4.

--- 19-array_methods/4/app.js
+++ 19-array_methods/4/app.js
@@ -1,3 +1,27 @@
-
 /* 4. Заполнять массив числами бесконечно до тех пор, пока длина массива не будет
 равна 5. Вывести массив в консоль (цикл for, while) */
+let array1 = [];
+let array2 = [];
+
+
+let i = 0;
+
+while (array1.length !== 5) {
+  array1.push(i);
+  i++;
+}
+
+console.log(array1);
+
+for (let i =0; i<20; i++){
+/* for (let i =0; ; i++){ */
+    while (array2.length!==5) {
+        array2.push(i);
+        console.log(i);
+    }
+    if(i>20)
+        break;
+
+}
+
+console.log(array2);
