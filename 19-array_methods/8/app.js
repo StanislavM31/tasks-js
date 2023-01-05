948ff9ee81030a478d1fb422d7f9572799531038
Stanislav Minko <minkostas@gmail.com>
2023-01-05 23:57:58 +0300

    feat: 19-array-method_8.1

--- 19-array_methods/8/app.js
+++ 19-array_methods/8/app.js
@@ -1,25 +1,22 @@
-
 /* На входе n – количество элементов массива. Далее производится заполнение
 массива с клавиатуры. Если все элементы массива – числа, то вывести логическое
 trur, в противном случае falsr. Evrry, forEach */
 
+let n = prompt("enter count of elements...");
 let arr = [];
+let arr1 = [];
+for (let i = 0; i < n; i++) {
+  let temp = prompt();
+  /*    while(isNaN(temp=prompt(' give me the element!...:'))){
+      alert('мне нужны только числа!')
+   } */
 
-let n = prompt('count of elements...');
-
-for (let i =0; i<n; i++){
-    arr.push(+prompt('enter the element'));
+  arr.push(+temp);
 }
+console.log(arr);
+let rez = arr.every(function (element) {
 
-
-
-
-let rez = arr.every(function (el) {
-     if(typeof el == 'number'){
-        return true;
-     } else {
-        return false;
-     }
+   return !isNaN(element);
 });
 
 console.log(rez);
