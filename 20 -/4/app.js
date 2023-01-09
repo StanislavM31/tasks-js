da440e0c06948912a18c54c3c6613c77bf2b5e82
Stanislav Minko <minkostas@gmail.com>
2023-01-10 02:19:29 +0300

    feat: 20_4

--- 20 -/4/app.js
+++ 20 -/4/app.js
@@ -1,1 +1,27 @@
 /* Палиндром. Сравнить с развернутой строкой не используя reverse. For */
+
+let str = prompt('enter string');
+let temp = '';
+for (let i=str.length-1; i>=0; i--){
+    console.log(str[i]);
+    temp += str[i];
+    console.log(temp);
+}
+
+console.log(temp);
+if(temp==str){
+    console.log("Палиндром!!");
+}
+else{
+    console.log("не палиндром");
+}
+
+switch (temp) {
+    case str:
+        console.log("Палиндром!!");
+        break;
+
+    default:
+        console.log("не палиндром");
+        break;
+}
