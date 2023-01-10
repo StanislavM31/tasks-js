b4a48c9e622031dc2773a18e82abbd346de0dd78
Stanislav Minko <minkostas@gmail.com>
2023-01-10 04:10:22 +0300

    feat: 20_2

--- 20 -/2/app.js
+++ 20 -/2/app.js
@@ -1,1 +1,17 @@
 /* 2. Найти максимальное число динамичного массива. filter  */
+
+let arr = [2, 3, 4, 100, 2, 500, 17, 1];
+/* let l = prompt("Enter size of array"); */
+
+/* for (let i = 0; i < l; i++) {
+  arr.push(+prompt("Enter elem of arr"));
+} */
+
+let max = arr[0];
+
+arr.forEach(function (el) {
+  if (el > max) {
+    max = el;
+  }
+});
+console.log(max, arr);
