83184ad236c504bc60d5d2e27d477cb86181df4f
Stanislav Minko <minkostas@gmail.com>
2023-01-14 00:44:18 +0300

    feat: 22-functions_5

--- 22 - functions/5/app.js
+++ 22 - functions/5/app.js
@@ -1,19 +1,23 @@
 /*
-5. Создать массив гласных из динамичного массива. Filter
- */
-let l = +prompt("Enter array length array");
+5. На входе n – количество элементов массива. Далее производится заполнение
+массива с клавиатуры. Реализуйте 2 функции. Первая для формирования массива.
+Вторая для нахождения количества элементов массива
+*/
 let arr = [];
-let vowels = 'аеийоуэюя';
-let rez=[];
-for (let i = 0; i < l; i++) {
-  arr.push(prompt("Enter elem of arr (БУКВА)"));
+let size = prompt("Ведите размер массива");
+
+let doArr = (d, s) => {
+
+  for (let i = 0; i < size; i++) {
+    const element = prompt('Введите элемент массива');
+    d.push(element);
+  }
+  console.log(d);
+  return d;
 }
-vowels = vowels.split('');
-arr.forEach(function(element, i){
-  rez = vowels.filter(function (el) {
-    if (element==el) {
-      return el;
-    }
-});
-});
-console.log(rez, arr);
+
+let getLength = (a) =>{
+  return a.length;
+}
+
+console.log( getLength(doArr(arr, size)));
