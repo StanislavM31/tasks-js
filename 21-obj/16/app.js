590029c1f2fbe4a97c1effe0b2c66e82d2b0fad2
Stanislav Minko <minkostas@gmail.com>
2023-01-13 21:18:25 +0300

    feat: 21-obj_16

---
+++ 21-obj/16/app.js
@@ -0,0 +1,39 @@
+/*
+16. На входе объект из 5 свойств. Значения вводим с клавиатуры. Необходимо
+проитерировать значения объекта и сформировать на основе этих чисел массив
+[
+{ "id": "javascript", "label": "JavaScript", "category": "programmingLanguages", "priority": 1 },
+{ "id": "typescript", "label": "TypeScript", "category": "programmingLanguages", "priority": 1 },
+{ "id": "sql", "label": "SQL", "category": "programmingLanguages", "priority": 2 },
+{ "id": "java", "label": "Java", "category": "programmingLanguages", "priority": 3 },
+{ "id": "go", "label": "GO", "category": "programmingLanguages", "priority": 3 }
+]
+*/
+
+const arr = [
+    { id: "javascript",
+     label: "JavaScript",
+     category: "programmingLanguages",
+     priority: 1
+    },
+    { id: "typescript",
+     label: "TypeScript",
+     category: "programmingLanguages",
+     priority: 1
+    },
+    { id: "sql",
+     label: "SQL",
+     category: "programmingLanguages",
+     priority: 2
+    },
+    { id: "java",
+     label: "Java",
+     category: "programmingLanguages",
+     priority: 3
+    },
+    { id: "go",
+     label: "GO",
+     category: "programmingLanguages",
+     priority: 3
+    }
+];
