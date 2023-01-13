2e035e694a8b77362ea45a4bc6c1551aab0dfdec
Stanislav Minko <minkostas@gmail.com>
2023-01-14 02:20:20 +0300

    feat: 22-functions_6

--- 22 - functions/6/app.js
+++ 22 - functions/6/app.js
@@ -1,24 +1,35 @@
-/* На входе n – количество элементов массива. Далее производится заполнение
-массива с клавиатуры. Необходимо вывести trur, если хотя бы 1 элемент массива –
-число. Somr, forEach */
-
+/*
+6. На входе массив. Реализуйте 2 функции. Первая для проверки, что в массиве
+только числа. Вторая для получения суммы всех элементов массива. Если
+результат функции проверки – true, то вызывать новую функцию, возвращающую
+сумму всех элементов массива
+ */
 
 let arr = [];
-let l = prompt('length of arr:...');
-
+let l = prompt("length of arr:...");
 
 for (let i = 0; i < l; i++) {
-
-    arr.push(+prompt('введите элемент массива'));
-
+  arr.push(prompt("введите элемент массива"));
 }
 
-console.log(
-    arr.some(function(elem) {
-        if( typeof elem === 'number'){
-            return true;
-        } else {
-            return false
-        };
-    })
-);
+const isDigit = (a) => {
+  return a.every((el) => {
+     if(!isNaN(el) == true){
+        return true;
+     }
+  });
+};
+
+const summa = s =>{
+    return s.reduce((sum, elem)=>{
+        return sum+ +elem;
+    }, 0)
+}
+
+if(isDigit(arr)==true){
+    console.log((arr));
+    console.log(summa(arr));
+} else{
+    console.log('в массиве есть строки. Не могу сложить');
+}
+
