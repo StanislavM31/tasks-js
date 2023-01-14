34cbebd89a2ae05cdfe36fd3af742787b06ec4ed
Stanislav Minko <minkostas@gmail.com>
2023-01-14 17:10:53 +0300

    feat: 22-functions_7

--- 22 - functions/7/app.js
+++ 22 - functions/7/app.js
@@ -5,3 +5,41 @@
 возвращающую конкатенацию всех строчных элементов массива
 
  */
+
+let arr = [];
+const doArray = a =>{
+    let l = +prompt("Введите размер массива...");
+
+    for (let i = 0; i < l; i++) {
+      const element = prompt("введите элемент массива...");
+      a.push(element);
+    }
+    if(check(a)==true){
+        console.log(sum(a));
+    } else {
+        console.log(concat(a));
+    }
+}
+
+const check = (c) => {
+    return c.every(function (el) {
+        console.log(isNaN(el));
+    return !isNaN(el);
+  });
+};
+const sum = s => {
+    console.log('SUMMA');
+    return s.reduce(function (sum, el) {
+        return sum+ +el;
+    },0)
+}
+const concat = c =>{
+    console.log('CONCAT');
+    let str = "";
+    c.forEach(element => {
+        str+=element;
+    });
+    return str;
+}
+
+doArray(arr);
