221e3895940d8ae806140ae19c93ce87a43f0588
Stanislav Minko <minkostas@gmail.com>
2023-01-23 02:07:13 +0300

    refactor: 23-context_7

--- 23 - context/7/app.js
+++ 23 - context/7/app.js
@@ -12,31 +12,3 @@ console.log(foo('coca'));
 console.log(foo('cola'));
 console.log(foo(' is good'));
 
-const obj = {
-    test: function(){
-        console.log(this);
-        console.log(obj.id);
-        console.log(this.id);
-        console.log(this.exist);
-    },
-    id: 777,
-    exist: true,
-    func: ()=>{
-        console.log(this);
-    }
-}
-
-obj.func();//window
-obj.test();
-
-function test(){
-    console.log('---');
-    return function(){
-        console.log('+++++');
-        console.log('+++++');
-        console.log('+++++');
-
-    }
-}
-
-let x = test();
