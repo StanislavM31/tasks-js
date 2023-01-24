b50c0bbf93b3eb2df7c11c6e320f57999155ca73
Stanislav Minko <minkostas@gmail.com>
2023-01-24 23:59:17 +0300

    refactor: 23-context_14.1

--- 23 - context/14.1/app.js
+++ 23 - context/14.1/app.js
@@ -1,3 +1,18 @@
 /*
-14. *Вычислите сумму массива целых чисел статичного массива используя рекурсию
+14.1 *Вычислите сумму массива целых чисел статичного массива используя рекурсию
 */
+
+const arr = [10,20,30,40,50];
+let i = 0;
+let s = 0;
+
+function sumArr(array) {
+  if(array[i]== undefined){
+    return s;
+  }
+  s+=array[i];
+  i++;
+  return sumArr(array);
+}
+
+console.log(sumArr(arr));
