d6cad6c4bdcb95369d289f4902019d91e845930f
Stanislav Minko <minkostas@gmail.com>
2023-01-30 23:04:05 +0300

    feat: 26-error_10

--- 26-error/10/app.js
+++ 26-error/10/app.js
@@ -3,3 +3,18 @@
 найти в данной почте “@”. Если же есть, то вывести булевое true, в противном
 случае бросить исключение и его обработать
 */
+
+let m = 'hello@mail.ru';
+
+function isValid(mail){
+    try{
+        if(!mail.includes("@")){
+            throw new Error('email is not valid')
+        }
+        return true;
+    } catch(error){
+        return error.message;
+    }
+}
+
+console.log(isValid(m));
