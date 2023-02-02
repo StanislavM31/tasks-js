ea97e059b421e02af3031b9d8051181527b754ec
Stanislav Minko <minkostas@gmail.com>
2023-02-02 15:28:21 +0300

    feat: 28_NumberSystem_1

--- 28-NumberSystem/1/app.js
+++ 28-NumberSystem/1/app.js
@@ -3,7 +3,38 @@
 всех положительных чисел. Добавить проверки
 */
 
-let arr = [1,-1,2,-3,4,5,-10];
+
+function doArray(){
+    let size = prompt('введите массив');
+let array = [];
+    for (let i = 0; i < size; i++) {
+        const element = +prompt('Элемент...');
+        array.push(element);
+    }
+    return array;
+}
+
 
 function count(array) {
+    let sum = 0;
+    try {
+        for (let i = 0; i < array.length; i++) {
+            if(array.length<1){
+                throw new Error('Массив пуст')
+            }
+            if(Number.isNaN(array[i])){
+                throw new Error('В массиве буква!')
+            }
+            if(array[i]>0){
+                sum+=array[i];
+            }
+        }
+        return sum;
+
+    } catch (error) {
+        return error.message;
+    }
 }
+
+let r = doArray();
+console.log(count(r));
