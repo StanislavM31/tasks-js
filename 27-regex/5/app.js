d400124b6e0cb7d628c0a0bfa6ac13747674cfd9
Stanislav Minko <minkostas@gmail.com>
2023-02-04 22:57:26 +0300

    feat: 27-regex

--- 27-regex/5/app.js
+++ 27-regex/5/app.js
@@ -3,10 +3,10 @@
 для данной строки. Если же строка подходит под регулярное выражение, то
 вывести булевое true, в противном случае бросить исключение и обработать
 */
-let str = 'http://goldbusinessnet.com/';
+let str = 'http://goldbusinessnet.com';
 function check(string){
     try {
-        if(!/^http:\/\/[a-z]+.[a-z]+\/$/gm.test(string)) throw new Error('не корректный url');
+        if(!/^http:\/\/[a-z]+.[a-z]+$/gm.test(string)) throw new Error('не корректный url');
 
         return true;
     } catch (error) {
--- 27-regex/8/app.js
+++ 27-regex/8/app.js
@@ -1,40 +1,6 @@
 /*
-8. Реализуйте функцию, которая принимает массив последовательных
-(возрастающих) букв и возвращает отсутствующую в массиве. Добавить проверки
-["a","b","c","d","f"] -> "e"
-["O","Q","R","S"] -> "P"
+8. Дана строка состоящая из чисел, букв, специальных символов. Необходимо
+оставить в строке только буквенные символы используя регулярные выражения.
+Если же длина исходной строки изменилась, вывести true, в противном случае
+бросить исключение и обработать
 */
-
-let arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-let arr_EN = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-function doArray(){
-    for (let i = 'a'.charCodeAt(0); i < 'z'.charCodeAt(0); i++) {
-        let temp1 = String.fromCharCode(i);
-        let temp2 = String.fromCharCode(i).toUpperCase();
-    }
-}
-
-function fixArray(array){
-    try {
-        let res;
-        for (let i = 0; i < array.length; i++) {
-            if(array[i+1]!== undefined){
-                const element = array[i+1].charCodeAt(0);
-                const prev = array[i].charCodeAt(0);
-                let dif = element-prev;
-                if(dif>1){
-                    res = String.fromCharCode(prev+1);
-                    console.log(res);
-                    throw new Error(`'массив непоследовательный' ${res}`);
-                }
-            }
-
-        }
-        throw new Error(`массив последовательный`);
-    } catch (error) {
-        error.message;
-    }
-}
-let a = ["a","b","d","f"];
-console.log(fixArray(a));
