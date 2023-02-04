984b45c28a2bbc302bfacb3090ee34744e8cbf0e
Stanislav Minko <minkostas@gmail.com>
2023-02-04 22:03:24 +0300

    feat: 27-regex_5

--- 27-regex/5/app.js
+++ 27-regex/5/app.js
@@ -1,3 +1,17 @@
 /*
-
+5. На вход подается строка в виде url. Необходимо написать регулярное выражение
+для данной строки. Если же строка подходит под регулярное выражение, то
+вывести булевое true, в противном случае бросить исключение и обработать
 */
+let str = 'http://goldbusinessnet.com/';
+function check(string){
+    try {
+        if(!/^http:\/\/[a-z]+.[a-z]+\/$/gm.test(string)) throw new Error('не корректный url');
+
+        return true;
+    } catch (error) {
+        error.message;
+    }
+}
+
+console.log(check(str));
