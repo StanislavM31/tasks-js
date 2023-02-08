83512f9a0170cf7ea112cca0c566a5713fcd396d
Stanislav Minko <minkostas@gmail.com>
2023-02-08 17:10:44 +0300

    feat: 30-eventListener_6

--- 30-eventListener/6/app.js
+++ 30-eventListener/6/app.js
@@ -1,3 +1,12 @@
-/*
 
+/*
+Необходимо отобразить кнопку и инпут со значением по умолчанию.
+По клику на кнопку заменить значение инпута
 */
+
+const inp = document.querySelector('input');
+console.log(inp);
+
+inp.addEventListener('click', ()=>{
+    inp.value="значение НЕ по умолчанию";
+})
