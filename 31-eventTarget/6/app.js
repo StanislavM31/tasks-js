fa7192fa45a8e73bea25ad41948b90320c58587c
Stanislav Minko <minkostas@gmail.com>
2023-02-13 02:05:27 +0300

    feat: 31-eventTarget_6

--- 31-eventTarget/6/app.js
+++ 31-eventTarget/6/app.js
@@ -2,3 +2,12 @@
 Добавьте картинку на страницу. При наведении на картинку мышкой необходимо ее изменять на другую картинку.
 Но как только мышка снова отходит в сторону, то возвращается первая картинка
 */
+
+let img = document.querySelector('.img');
+
+img.addEventListener('mouseover', function(){
+    this.style = `background-image: url(./assets/hs-2.png)`
+})
+img.addEventListener('mouseout', function(event){
+    event.target.style = `background-image: url(./assets/hs-1.png)`
+})
