9d0ce7ece17e36f962ee652c5efa2811e6bad123
Stanislav Minko <minkostas@gmail.com>
2023-02-13 12:16:19 +0300

    feat: 33-objectMethods_6

--- 33 - objectMethods/6/app.js
+++ 33 - objectMethods/6/app.js
@@ -1,14 +1,26 @@
 /*
-Найти квадратный корень числа. Если же число без плавающей точки, то просто
-вывести его. В противном случае необходимо округлить число до целых. Добавить
-проверку на ввод только чисел
+6. На входе объект и число n, символизирующее количество пар ключ-значение.
+Ключи - автоинкремент (генерируется автоматически от 1 до n). Значение каждого
+ключа – рандомное число от 1 до 100. Вывести каждое значение объекта с
+помощью values
 */
 
-let n = +prompt('введите число:');
-let x = Math.sqrt(n);
+let n_ = 15;
 
-if(x%(Math.floor(x))==0){
-    console.log(`${x} целое число`)
-} else {
-    console.log(`${Math.floor(x)}`);
+function doObj(n) {
+  let o = {};
+  for (let i = 0; i < n; i++) {
+    o[i] = Math.floor(Math.random() * 100);
+  }
+  return o;
 }
+let obj = doObj(n_);
+
+console.log(obj);
+
+function doArr(obj_){
+    let arr = Object.values(obj_);
+    arr.forEach(el=>console.log(el))
+}
+
+doArr(obj);
