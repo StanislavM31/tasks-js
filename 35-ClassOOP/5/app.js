34e7bda089e84835629b6d21c0db6661cc718fad
Stanislav Minko <minkostas@gmail.com>
2023-02-19 22:30:42 +0300

    feat: 35_class_5

--- 35-ClassOOP/5/app.js
+++ 35-ClassOOP/5/app.js
@@ -1,8 +1,25 @@
-/* 5. Выведите столбец четных чисел в промежутке от 0 до 100 (for)
- */
+/*
+5. Реализуйте класс Anagram (Анаграмма это слово или фраза , полученный путем
+замены букв исходного слова или фразы). Создать функцию для вывода ряда true,
+если слова являются анаграммами. Добавить проверки на ввод.
+*/
 
-for (let i = 0; i <= 100; i++) {
-  if (i % 2 == 0) {
-    console.log(i);
+class Anagram {
+  constructor(str) {
+    this.str = str;
+    this.isAnagramOrNot = this.isAnagram(str)
+  }
+  isAnagram(word) {
+    /* this.str = str; */
+    if (word.split("").reverse().join("") == word) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
+let word = prompt("введите анаграмму");
+let anagram = new Anagram(word);
+console.log(anagram);
+console.log(anagram.isAnagramOrNot);
+console.log(anagram.isAnagram(word));
