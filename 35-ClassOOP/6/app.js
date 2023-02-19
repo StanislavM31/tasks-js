1b189fe902a59621cf6f723cb518952d7a2f2f51
Stanislav Minko <minkostas@gmail.com>
2023-02-19 16:44:39 +0300

    feat: 35_class_7

--- 35-ClassOOP/6/app.js
+++ 35-ClassOOP/6/app.js
@@ -1,14 +1,38 @@
 /*
-6. С помощью цикла while найдите сумму чисел от 1 до 100.
+6. Реализуйте класс ServerGetAll. Обязательными функциями считаются функции
+controller, service, repository. Цепочка взаимодействия между методами
+следующая:
+controller -> service -> repository, где:
+controller – функция, принимающая данные. Принимает json
+service – функция проверки на то что с repository вернулось значение
+repository – функция, симулирующая БД. Хранит массив данных. Взаимодействие с
+этим массивом осуществляется только в repository. Массив находится в
+приложении
+Задание:
+Необходимо вывести в консоль весь массив
+*/
 
- */
 
-let sum = 0,
-  i = 0;
+  class ServerGetAll {
+    controller(){
+      const data = this.service();
+      return data;
+    }
+    service(){
+      const data = this.repository();
+      return data;
+    }
+    repository(){
+      let arr = [
+        { "id": "javascript", "label": "JavaScript", "category": "programmingLanguages", "priority": 1 },
+        { "id": "typescript", "label": "TypeScript", "category": "programmingLanguages", "priority": 1 },
+        { "id": "sql", "label": "SQL", "category": "programmingLanguages", "priority": 2 },
+        { "id": "java", "label": "Java", "category": "programmingLanguages", "priority": 3 },
+        { "id": "go", "label": "GO", "category": "programmingLanguages", "priority": 3 }
+        ]
+        return arr;
+    }
+  }
 
-while (i <= 100) {
-  sum += i;
-  i++;
-}
-
-console.log(sum);
+  const serverGetAll = new ServerGetAll();
+  console.log(serverGetAll.controller());
