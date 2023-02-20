bd72bdff5048c94621dcaf28337f6e0bd546dace
Stanislav Minko <minkostas@gmail.com>
2023-02-20 03:01:56 +0300

    refactor: 35_class_4

--- 35-ClassOOP/4/app.js
+++ 35-ClassOOP/4/app.js
@@ -8,5 +8,26 @@
 class MathСalculation{
     constructor(n){
         this.n = n;
+        this.arr = this.createArray(n);
+        this.filtered = this.filterArray(this.arr)
+    }
+    createArray(length){
+        const array = [];
+        for (let i = 0; i < length; i++) {
+            array.push(Math.floor(Math.random()*100)) ;
+        }
+        return array;
+    }
+    filterArray(array1){
+        let filtered = array1.filter(el=>{
+            if(el%2==0){
+                return el;
+            }
+        })``
+        return filtered.reduce((a,b)=>a+b);
     }
 }
+
+let mathСalculation = new MathСalculation(5);
+console.log(mathСalculation);
+console.log(mathСalculation.filtered);
