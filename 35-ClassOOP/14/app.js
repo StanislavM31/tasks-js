48854807e0b0525a6fbbf6f7b48a1f193ee772fd
Stanislav Minko <minkostas@gmail.com>
2023-02-22 04:10:54 +0300

    feat: 35-class_14

--- 35-ClassOOP/14/app.js
+++ 35-ClassOOP/14/app.js
@@ -18,11 +18,11 @@ repository – функция, симулирующая БД. Хранит ма�
 Если совпадения по id нет – ошибка. Добавить проверки
 */
 
-class ServerPut{
-  middleware(object){
+class ServerPut {
+  middleware(object) {
     return this.controller(object);
   }
-  controller(obj){
+  controller(obj) {
     let object = JSON.parse(obj);
     try {
       let data = this.service(object);
@@ -31,31 +31,42 @@ class ServerPut{
       error.message;
     }
   }
-  service(object){
-    return this.repository(object)
+  service(object) {
+    return this.repository(object);
   }
-  repository(object){
+  repository(object) {
     let arr = [
       { "id": "javascript", "label": "JavaScript", "category": "programmingLanguages", "priority": 1 },
       { "id": "typescript", "label": "TypeScript", "category": "programmingLanguages", "priority": 1 },
       { "id": "sql", "label": "SQL", "category": "programmingLanguages", "priority": 2 },
       { "id": "java", "label": "Java", "category": "programmingLanguages", "priority": 3 },
       { "id": "go", "label": "GO", "category": "programmingLanguages", "priority": 3 }
-      ]
-let find = arr.filter(function(elOfArr){
-  if(elOfArr.id == object.id){
-    return true;
-  }
-})
-console.log(find);
+    ];
+    let find = arr.filter(function (elOfArr) {
+      if (elOfArr.id == object.id) {
+        return true;
+      }
+    });
+    console.log(find);
+    if (!find.length) {
+      /* arr = [{id:object.label.toLowerCase(), ...object}, ...arr] */
+      throw new Error("ошибка: Cовпадений нет");
+    } else{
+      let finalArr = arr.filter(el=>{
+        if(el.id !== object.id){
+          return true;
+        }
 
-      return arr;
+      })
+      return [object, ...finalArr];
+
+    }
+
   }
 }
-let element = `{
-  "label": "COBOL", "category": "programmingLanguages", "priority": 1
-  }`;
-let serverPut = new ServerPut(element);
-let m = serverPut.middleware();
-console.log(m);
 
+let elem =`{"id": "javascript", "label": "JavaScript", "category": "programmingLanguages", "priority": 999}`;
+
+let serverPut = new ServerPut();
+let m = serverPut.middleware(elem);
+console.log(m);
