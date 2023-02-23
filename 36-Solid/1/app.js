5908d6db3c1abf798dec4ca516da3172a1f4c3d9
Stanislav Minko <minkostas@gmail.com>
2023-02-23 03:33:54 +0300

    feat: 36-solid_1_doMinus

--- 36-Solid/1/app.js
+++ 36-Solid/1/app.js
@@ -8,18 +8,26 @@ class DomHtml {
     this.input1 = input1;
     this.input2 = input2;
     this.generateEvent();
+    this.res = document.querySelectorAll('.result');
   }
   doPlus() {
-    let res = document.querySelector('.result')
-    res.innerHTML = +this.input1.value + +this.input2.value;
+    console.log(this.res);
+    this.res[0].innerHTML =  `результат сложения: ${+this.input1.value + +this.input2.value}`;
+    console.log(this.res[0]);
+    console.log(this.res[0].value);
   }
-  doMinus() {}
+  doMinus() {
+    this.res[1].innerHTML =  `результат вычитания: ${+this.input1.value - +this.input2.value}`;
+    console.log(this.res[1]);
+    console.log(this.res[1].value);
+  }
   doMyltiply() {}
   doDivide() {}
   generateEvent() {
     let btn = document.querySelector("button");
     btn.addEventListener("click", () => {
-        this.doPlus()
+        this.doPlus();
+        this.doMinus();
     });
   }
 }
@@ -28,4 +36,5 @@ let inp1 = document.querySelector(".input1");
 let inp2 = document.querySelector(".input2");
 
 let domHTML = new DomHtml(inp1, inp2);
+
 
