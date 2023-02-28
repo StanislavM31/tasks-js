adefb189b8843a40a97dc4442460d4ee614df81f
Stanislav Minko <minkostas@gmail.com>
2023-02-28 22:04:30 +0300

    feat: 36-solid_9

--- 36-Solid/9/app.js
+++ 36-Solid/9/app.js
@@ -1,45 +1,23 @@
 /*
-9. Дополнить класс Validator. Добавить метод isURL для проверки на url.
+9. Реализуйте класс Сonversion, сдержащий метод do DecimalNumber, преобразующий
+все числа 10 системы счисления в 2
 */
-let u = `https://habr.com`;
 
-class Validator{
-  constructor(url){
-    this.url = url
-    this.isURL = this.isURL()
-  }
-
-  isURL(){
-    let reg = new RegExp(/^[a-z]{4,5}:\/\/[a-zA-Z]+\.[a-z]{1,3}$/gm);
-    try{
-        if(reg.test(this.url)){
-            return true
-        } else {
-            throw new Error('не валидный URL!')
-        }
-    } catch(error){
-        error.message;
+class DecimalNumber {
+  toDecimal(n) {
+    try {
+      let result = "";
+      while (n >= 1) {
+        result += n % 2;
+        n = n / 2;
+      }
+      return result;
+    } catch (err) {
+      alert(err.message);
     }
   }
 }
 
-let validator = new Validator('https://habr.com');
-console.log(validator.isURL);
-
-class UniValidator{
-    isURL(url){
-        let reg = new RegExp(/^[a-z]{4,5}:\/\/[a-zA-Z]+\.[a-z]{1,3}$/gm);
-        try{
-            if(reg.test(url)){
-                return true
-            } else {
-                throw new Error('uniValidator: не валидный URL!')
-            }
-        } catch(error){
-            error.message;
-        }
-      }
-}
-let other_url = `https://habr.com`;
-let uniValidator = new UniValidator();
-console.log(uniValidator.isURL(other_url));
+let decimalNumber = new DecimalNumber();
+let decimal = decimalNumber.toDecimal(8);
+console.log(decimal);
