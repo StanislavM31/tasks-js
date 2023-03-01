1359a9fdd3866752cf3b483664a88152045e7639
Stanislav Minko <minkostas@gmail.com>
2023-03-01 11:39:56 +0300

    feat: 37-extends_2(1)|(1)2

--- 37-extends/2/app.js
+++ 37-extends/2/app.js
@@ -1,37 +1,56 @@
 /*
-На входе 7 чисел. Все эти значения пользователь вводит с клавиатуры и заполяет
-5 первыми значениями массив. Далее необходимо создать массив из тех
-значений, которые являются числами и НЕ равны 2 другим введенным. Если
-длина массива равна 0, то вывести ‘Массив пуст’. Добавить проверку на ввод
-только чисел после каждого введенного элемента. forEach
+2.(1). Классы Person и Customer. Напишите класс Person с атрибутами данных для
+имени, адреса и телефонного номера человека. Затем напишите класс Customer
+(Клиент), который является подклассом класса Person. Класс Customer должен
+иметь поле для номера клиента и атрибут булевых данных,
+указывающий, хочет ли клиент быть в списке рассылки или нет.
+Продемонстрируйте экземпляр класса Customer в простой программе
 */
-const value = 5;
-let x = prompt('enter x value');
-let y = prompt('enter y value');
 
-let arr = [];
-
-
-
-for( let i =0; i< value; i++){
-
-    arr.push(prompt('enter value'));
+class Person {
+  name;
+  adress;
+  phone;
+  setName(n) {
+    this.name = n;
+  }
+  setAdress(a) {
+    this.adress = a;
+  }
+  setPhone(p) {
+    this.phone = p;
+  }
+  getName() {
+    return this.name;
+  }
+  getAdress() {
+    return this.adress;
+  }
+  getPhone() {
+    return this.phone;
+  }
+}
+class Customer extends Person {
+  idClient;
+  bool;
+  setIdClient(id) {
+    this.idClient = id;
+  }
+  setBool(b){
+    this.bool = b;
+  }
+  getIdClient = () => this.idClient;
+  getBool = () => this.bool;
 }
 
-/* arr.forEach(function(elem){
-    if(!isNaN(elem) && elem !== x && elem !== y){
-        rez.push(elem);
-    }
-}) */
-
-let rez = arr.filter(function(elem){
-    if(!isNaN(elem) && elem !==x && elem !==y){
-        return false;
-    }
-})
-
-if(rez.length===0){
-    console.log('empty');
-} else {
-    console.log(rez);
-}
+let customer = new Customer();
+customer.setName('Gena');
+customer.setAdress('Sennitsa');
+customer.setPhone('+375291114471');
+customer.setIdClient(111234);
+customer.setBool(true);
+console.log(customer.getName());
+console.log(customer.getPhone());
+console.log(customer.getAdress());
+console.log(customer.getIdClient());
+console.log(customer.getBool());
