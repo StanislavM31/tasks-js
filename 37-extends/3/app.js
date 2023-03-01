8e31a6d09163989fb179ecae67b5a943cfe9c2f1
Stanislav Minko <minkostas@gmail.com>
2023-03-01 11:54:34 +0300

    feat: 37-extends_2

--- 37-extends/3/app.js
+++ 37-extends/3/app.js
@@ -1,45 +1,19 @@
 /*
-На входе n – количество элементов массива. Далее производится заполнение
-массива с клавиатуры. Необходимо вывести значения массива без дублирования.
-Если длина массива равна 0, то вывести ‘Массив пуст’. Добавить проверку на ввод
-только чисел после каждого введенного элемента.
-1, 1, 3, 4, 4, 4, 2, 5 -> 1, 2, 3, 4, 5
+2. Реализуйте класс Singer, который будет наследовать от класса Worker. Класс
+Worker состоит из: свойства Name, свойства Surname, метода GetFullName(), с
+помощью которого можно вывести одновременно имя и фамилию; Класс Singer
+наследует Name, Surname, также состоит из свойств Years, Birth, Style и метода
+GetMoreInfo(), который выводит информацию о певце: Years, Birth, Style.
+Необходимо вызвать метод GetMoreInfo
 */
-const a = prompt("amount of elem");
-const arr = [];
-let rez = [];
 
-for (let i = 0; i < a; i++) {
-  let temp = prompt("enter elem");
-  if (!isNaN(temp)) {
-    arr.push(+temp);
+class Worker{
+  name;
+  surname;
+  getFullName(){
+    return `${name} ${surname}`;
   }
-}
-
-/* for( let i = 0 ; i<arr.length; i++){
-    if(!rez.includes(arr[i])){
-        rez.push(arr[i]);
-    }
-} */
-
-/* arr.forEach(function (elem) {
-    if(!rez.includes(elem)){
-        rez.push(elem);
-    }
-});
- */
-
-/* rez = arr.filter(function(elem){
-    if(!rez.includes(elem)){
-        rez.push(elem);
-       return true;
-    }
-}) */
-
-
-
-if (rez.length === 0) {
-  console.log("empty");
-} else {
-  console.log(rez);
+  setName = (n) =>{
+    this.name = n;
+  }
 }
