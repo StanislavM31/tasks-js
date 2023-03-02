72cd12303c9b2625ec3e82a94cee0b46469fbedd
Stanislav Minko <minkostas@gmail.com>
2023-03-02 23:55:48 +0300

    feat: 37-extends_4

--- 37-extends/3/app.js
+++ 37-extends/3/app.js
@@ -7,13 +7,49 @@ GetMoreInfo(), который выводит информацию о певце:
 Необходимо вызвать метод GetMoreInfo
 */
 
-class Worker{
-  name;
-  surname;
-  getFullName(){
-    return `${name} ${surname}`;
-  }
-  setName = (n) =>{
+class Worker {
+  /*   name;
+  surname; */
+  setName = (n) => {
     this.name = n;
+  };
+  setSurname = (s) => {
+    this.surname = s;
+  };
+  getFullName() {
+    let str = `${this.name} ${this.surname}`;
+    return str;
   }
 }
+
+class Singer extends Worker {
+  years;
+  birth;
+  style;
+  getFullName() {
+    let str = `the EX worker is ${this.name} ${this.surname}`;
+    return str;
+  }
+  setYears = (y) => {
+    this.years = y;
+  };
+  setBirth = (b) => {
+    this.birth = b;
+  };
+  setStyle = (s) => {
+    this.style = s;
+  };
+  getMoreInfo() {
+    return `${this.name} ${this.surname} ${this.birth} ${this.years}  ${this.style} `;
+  }
+}
+
+let singer = new Singer();
+singer.setName("Frank");
+singer.setSurname("Sinatra");
+singer.setBirth(1915);
+singer.setYears(1960);
+singer.setStyle("jazz");
+console.log(singer.getFullName());
+console.log(singer.getMoreInfo());
+/* ыещзауыьлвпдлаьпаьпдавл */
