303adfa935f2cb60040e674d3c627a6953d4de4f
Stanislav Minko <minkostas@gmail.com>
2023-03-05 04:03:58 +0300

    feat: 35-class_11

--- 35-ClassOOP/11/app.js
+++ 35-ClassOOP/11/app.js
@@ -3,8 +3,11 @@
 телефона.
 */
 
-class validator{
+class Validator{
   isPhone(d){
-    
+    return /\+(375)[2-9]{2}[1-9]{7}$/gm.test(d);
   }
 }
+
+let validator = new Validator();
+console.log(validator.isPhone('+375447756871'));
