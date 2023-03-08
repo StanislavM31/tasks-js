d9151d0fa45466104ab7606938f676bed9f645b2
Stanislav Minko <minkostas@gmail.com>
2023-03-08 11:11:08 +0300

    feat: 31-eventTarget_8

--- 31-eventTarget/8/app.js
+++ 31-eventTarget/8/app.js
@@ -1,3 +1,20 @@
 /*
-
+8 *Дана таблица c 1 колонкой.
+Под таблицей сделайте форму (инпут и кнопка),
+с помощью которой можно будет добавить нового значение в таблицу.
 */
+
+let divTag = document.querySelector('.table');
+let input = document.createElement('input');
+let btn = document.createElement('button');
+
+btn.innerHTML = "Push";
+
+document.querySelector('body').append(input, btn);
+
+btn.addEventListener('click',()=>{
+    let el = document.createElement('p');
+    el.innerHTML = input.value;
+    divTag.append(el);
+    input.value = '';
+})
--- 38-polymorth-repeat/15/app.js
+++ 38-polymorth-repeat/15/app.js
@@ -1,12 +1,33 @@
 /*
-Пользователь вводит строку. Необходимо сделать проверку, что это не число и
-вывести длину строки
+5. Полиморфизм. Реализуйте класс NumberArray. NumberArray хранит функцию
+multArr, которая заполняет и возвращает массив из 5 рандомных элементов.
+Класс ConsoleArray является производным по отношению к базовому,
+родительскому NumberArray и содержит функцию multArr. Необходимо
+переопределить multArr из базового класса записав в переменную используя
+метод super. Далее посчитать произведение всех элементов массива используя
+reduce
 */
 
-let a = prompt('введите строку');
-
-if(isNaN(a)){
-    console.log(a.length);
-} else {
-    console.log('нужно ввести строку. Попробуйте еще раз...')
+class NumberArray{
+    multArr(){
+        let array = [];
+        for (let i = 0; i < 5; i++) {
+            const element = Math.round(Math.random()*100);
+            array.push(element);
+        }
+        console.log(array);
+        return array;
+    }
 }
+
+class ConsoleArray extends NumberArray{
+    multArr(){
+        let arr=super.multArr();
+        return arr.reduce((cur,next)=>{
+            return cur*next;
+        },1)
+    }
+}
+
+let consoleArray = new ConsoleArray();
+console.log(consoleArray.multArr());
