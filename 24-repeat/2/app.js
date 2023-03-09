d9f0d7e8cae940e9fdd95254ec47b412184933bd
Stanislav Minko <minkostas@gmail.com>
2023-03-09 19:48:37 +0300

    feat: 24-repeat_2

--- 24-repeat/2/app.js
+++ 24-repeat/2/app.js
@@ -1,6 +1,28 @@
-/* Пользователь вводит с клавиатуры 2 числа: основание степени и показатель
-степени. Необходимо возвести число во введенную степень*/
-const value = +prompt();
-const subvalue = +prompt();
+/*
+2. Напишите функцию которая принимает массив чисел и возвращает сумму всех
+элементов массива. Добавьте функцию проверки. Написать синтаксис 3 функций
+*/
 
-console.log(value ** subvalue);
+function doArray() {
+  let arr = [];
+  for (let i = 0; i < 5; i++) {
+    let temp = check(prompt("введите элемент массива"));
+    arr.push(+temp);
+  }
+  return sum(arr);
+}
+
+function check(x) {
+  while (isNaN(x)) {
+    alert("НЕ число!");
+    x = prompt("введите число");
+  }
+  return x;
+}
+function sum(arr) {
+  return arr.reduce((a,b) => {
+    return a + b;
+  }, 0);
+}
+
+console.log(doArray());
