d8f99605aab77f55cbe835bc4459a9c87ea78d1c
Stanislav Minko <minkostas@gmail.com>
2023-03-10 20:18:14 +0300

    feat: 24-repeat_4

--- 24-repeat/4/app.js
+++ 24-repeat/4/app.js
@@ -1,1 +1,21 @@
+/* 4. Напишите функцию, которая принимает массив чисел и находит максимальное
+значение
+ */
+let arr = [];
+for (let i = 0; i < 5; i++) {
+    let element = Math.floor(Math.random()*100);
+    arr.push(element);
+}
+console.log(arr);
 
+function foo(array){
+    let big=0;
+    for (let i = 0; i < array.length; i++) {
+        if(big<arr[i]){
+            big=arr[i];
+        }
+    }
+    console.log(big);
+}
+
+foo(arr);
