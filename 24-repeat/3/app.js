d8dc1016a92e4535fa774136cb73c2bcd21dab94
Stanislav Minko <minkostas@gmail.com>
2023-03-10 19:57:53 +0300

    feat: 24-repeat_3

--- 24-repeat/3/app.js
+++ 24-repeat/3/app.js
@@ -1,11 +1,20 @@
-/* Напишите программу, которая считывает длины двух катетов в прямоугольном
-треугольнике и выводит его площадь. Используйте формулу: S =
-1
-2
-∙a ∙ b
-
+/*
+3. Написать функцию которая принимает массив стирок и возвращает
+конкатенацию. Добавьте функцию проверки
 */
-const a = +prompt('катет 1');
-const b = +prompt('катет 2');
+let array = ['a', 'b','c','d','e'];
+let array1 = ['a', 'b',1 ,'d','e'];
+function foo(arr){
+    let string = '';
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if(isNaN(element)){
+            string+=element;
+        } else{
+            alert(`${[arr[i]]} не является строкой`)
+        }
+    }
+    return string
+}
 
-console.log(a / 2 * b);
+console.log(foo(array));;
