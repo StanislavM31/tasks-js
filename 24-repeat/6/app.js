331a51ef7030941b9a6100a31772686e3cd50809
Stanislav Minko <minkostas@gmail.com>
2023-03-12 02:07:21 +0300

    feat: 24-repeat_6

--- 24-repeat/6/app.js
+++ 24-repeat/6/app.js
@@ -1,4 +1,22 @@
 /*
 6. Напишите функцию которая принимает массив чисел и возвращает
-произведение всех элементов массива. 
+произведение всех элементов массива.
 */
+
+function foo(array){
+    for (let i = 0; i < 5; i++) {
+        const element = Math.floor(Math.random()*100);
+        array.push(element);
+    }
+    console.log(array);
+    return array;
+}
+
+function foo1(array){
+    return array.reduce((a,b)=>a+b,0);
+}
+
+let a = [];
+let t = foo(a);
+console.log(foo1(t));
+
