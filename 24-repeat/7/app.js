3b991551d48f853a920444505cb178a72b54850c
Stanislav Minko <minkostas@gmail.com>
2023-03-13 02:40:17 +0300

    feat: 24-repeat_7-context

--- 24-repeat/7/app.js
+++ 24-repeat/7/app.js
@@ -3,3 +3,19 @@
 */
 
 //bind, call, apply ??
+
+let obj = {
+    'a': 1,
+    'b': 2,
+    'c': 100
+}
+function foo(a){
+    console.log(`${this.c} and ${a}`);
+
+}
+function foo1(a){
+    console.log(`${a} and ${this.c}`);
+}
+
+foo.call(obj, 5);
+foo1.call(5, obj);
