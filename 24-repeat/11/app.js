e23e3b7766f6cb542872490e2c5e5501808e5321
Stanislav Minko <minkostas@gmail.com>
2023-03-16 13:32:32 +0300

    feat: 24-repeat_11

---
+++ 24-repeat/11/app.js
@@ -0,0 +1,21 @@
+/*
+
+*/
+
+function recursion(){
+
+    let f = recursion() {
+        let num = 0;
+        return f() {
+            num++;
+            return num;
+        }
+    }
+}
+
+    let func = recursion();
+
+    console.log(func());
+console.log(func());
+console.log(func());
+console.log(func());
