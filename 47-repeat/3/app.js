3263e60a6c8d08dc3a60813f25b46d5885e079e8
Stanislav Minko <minkostas@gmail.com>
2023-04-10 00:26:22 +0300

    47-repeat_3

--- 47-repeat/3/app.js
+++ 47-repeat/3/app.js
@@ -1,18 +1,25 @@
+/*
+3. Вы вводите числа в поле для ввода формируя и отображая массив.
+Необходимо также отображать только уникальные значения массива
+[1, 1, 4, 2, 3, 3] -> [4, 2];
+*/
 
-// 3. Вы вводите числа в поле для ввода формируя и отображая массив.
-// Необходимо также отображать только уникальные значения массива
-// [1, 1, 4, 2, 3, 3] -> [4, 2]
-
-let input = document.querySelector('input');
 let btn = document.querySelector('button');
+let inp = document.querySelector('.input11');
 let array = document.querySelector('.array');
-let uniq = document.querySelector('.chet');
-let everyInp = [];
+let uniq = document.querySelector('.uniq');
+let arr = [];
 
 btn.addEventListener('click', function(){
-    everyInp.push(input.value);
-    array.innerHTML = everyInp;
-    
+    let arrUniq = [];
+    arr.push(inp.value);
+
+    for(let i =0; i<arr.length; i++){
+        if(arr[i-1] != arr[i] && arr[i+1] != arr[i] ){
+                arrUniq.push(arr[i]);
+        }
+    }
+    array.innerHTML = `#[ ${arr} ] -> [ ${arrUniq} ]`;
 })
 
 
