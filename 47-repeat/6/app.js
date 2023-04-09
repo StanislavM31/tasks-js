14655ba4e88a4fa95aa6298660304f7a0881871d
Stanislav Minko <minkostas@gmail.com>
2023-04-09 23:08:14 +0300

    47-repeat_6

--- 47-repeat/6/app.js
+++ 47-repeat/6/app.js
@@ -2,3 +2,11 @@
 6. При наведении на картинку изменять ее
 */
 
+let img = document.querySelector('img');
+
+img.addEventListener('mouseover', function(){
+  img.src = "./H2.png";
+})
+img.addEventListener('mouseout', function(){
+  img.src = "./H1.png";
+})
