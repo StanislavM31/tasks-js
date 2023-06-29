0b8f304097ef8e95b58ea03df2cb98a7978cf63e
Stanislav Minko <minkostas@gmail.com>
2023-06-29 14:40:51 +0300

    typescript_64: tasks_1-5

---
+++ 064.Types/src/app5.ts
@@ -0,0 +1,16 @@
+/*
+5. Создайте массив чисел и найдите в нем все числа, которые делятся на 3 и не
+делятся на 2.
+*/
+
+let arr5:number[] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+
+/* let result5:number[] = arr5.map((el)=>{
+    if(el%3==0 && el%2 !==0) return el;
+})
+
+console.log(result5); */
+
+let result5 = arr5.filter(el=>el%3==0 && el%2 !==0);
+console.log(result5);
+
