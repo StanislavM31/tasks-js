23bdffcb60e4305f2708f758d655b367bb42b73f
Stanislav Minko <minkostas@gmail.com>
2023-07-18 11:12:49 +0300

    jest: 1-5

---
+++ 069.unitTestsJS/app.js
@@ -0,0 +1,102 @@
+/*
+1. Написать функцию которая принимает 2 числа и возводит 1 во 2 степень.
+Добавить необходимые проверки.
+Написать тест для функции
+*/
+
+function sum(a, b) {
+  try {
+    if (typeof a !== "number" || typeof b !== "number") {
+      throw new Error(`не число`);
+    }
+    return a ** b;
+  } catch (error) {
+    return error.message;
+  }
+}
+/* 2. Написать функцию которая принимает 2 числа и возвращает результат
+произведения 2 множителей. Добавить необходимые проверки.
+Написать тест для функции */
+
+function multi(a, b) {
+  try {
+    if (typeof a == "bigint" || typeof b == "bigint")
+      throw new Error(`неверный тип числа(bigInt)`);
+    if (typeof a !== "number" || typeof b !== "number")
+      throw new Error(`не число`);
+    return a * b;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+/*
+3. Написать функцию которая принимает массив чисел и находит сумму всех
+элементов. Добавить необходимые проверки.
+Написать тест для функции
+*/
+
+function sumOfArray(array) {
+  try {
+    if (!Array.isArray(array)) throw new Error(`не массив`);
+    if (!array.length) throw new Error("empty arr");
+    let bool = array.every((el) => typeof el == "number");
+    if (!bool) throw new Error(`массив содержит строку`);
+    return array.reduce((acc, next) => acc + next, 0);
+  } catch (error) {
+    return error.message;
+  }
+}
+/*
+4. У вас есть массив объектов вида приведенного в приложении. Необходимо
+вывести все товары, количество которых больше 10 и произведены в Германии
+Написать тест для функции
+[
+{ id: 1, title: 'Часы', count: 10, price: 500, producer: 'Германия' },
+{ id: 2, title: 'Смартфон', count: 33, price: 1500 , producer: 'Германия' },
+{ id: 3, title: 'Моноблок', count: 6, price: 2200, producer: 'Германия' },
+{ id: 4, title: 'Ноутбук', count: 13, price: 3000, producer: 'Китай' },
+{ id: 5, title: 'Планшет', count: 22, price: 2100, producer: 'Китай' },
+{ id: 6, title: 'Телевизор', count: 4, price: 4100, producer: 'Германия' },
+{ id: 7, title: 'Холодильник', count: 11, price: 2400, producer: 'Германия' }
+]
+*/
+function findProduct(array) {
+  try {
+    if (!Array.isArray(array)) throw new Error("not array");
+    if (!array.length) throw new Error("empty array");
+    const filtered = array.filter(
+      (el) => el.producer == "Германия" && el.count > 10
+    );
+    return filtered;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+/* 5. У вас есть массив строковых значений, состоящий из номеров телефонов.
+Необходимо вывести значения массива без дубликатов. Добавить необходимые
+проверки.
+Написать тест для функции */
+
+
+function uniqNumbers(array) {
+  try {
+    if(!Array.isArray(array)) throw new Error(`not an array`);
+    if(!array.length) throw new Error(`Empty array`);
+    const boolString = array.every(el=> typeof el == "string");
+    if(boolString==false) throw new Error(`не все элементы - строки`)
+    const boolRegex = array.every(el=> /^\+375+[29,33,44]+[0-9]{7}/gm.test(el));
+    if(boolRegex==false) throw new Error(`не все элементы - phone numbers`)
+    const uniq = [];
+    for (const element of array) {
+      if (!uniq.includes(element)) {
+        uniq.push(element);
+      }
+    }
+    return uniq;
+  } catch (error) {
+    return error.message;
+  }
+}
+module.exports = { sum, multi, sumOfArray, findProduct, uniqNumbers };
---
+++ 069.unitTestsJS/app.test.js
@@ -0,0 +1,119 @@
+const { sum, multi, sumOfArray, findProduct, uniqNumbers} = require("./app.js");
+
+describe("набор тестов 1", () => {
+  test("Succes", () => {
+    const result = sum(2, 2);
+    expect(result).toBe(4);
+  });
+
+  test("Succes", () => {
+    const result = sum(2, "string");
+    expect(result).toBe(`не число`);
+  });
+});
+
+describe("набор тестов 2", ()=>{
+    test("Успешно", ()=>{
+        const result = multi(2,10);
+        expect(result).toBe(20);
+    })
+    test("Успешно", ()=>{
+        const result = multi(2,"sss");
+        expect(result).toBe("не число")
+    })
+    test("Успешно", ()=>{
+        const result = multi(2, 144n);
+        expect(result).toBe(`неверный тип числа(bigInt)`);
+    })
+
+})
+
+describe("набор тестов 3", ()=>{
+    test("Success",()=>{
+        const result = sumOfArray([10,20,30]);
+        expect(result).toBe(60);
+    })
+    test("Success", ()=>{
+        const result = sumOfArray("string123");
+        expect(result).toBe("не массив");
+    })
+    test("Success", ()=>{
+        const result = sumOfArray([]);
+        expect(result).toBe('empty arr');
+    })
+    test("Success", ()=>{
+        const result = sumOfArray([10,10,"string"]);
+        expect(result).toBe('массив содержит строку');
+    })
+})
+let arr = [
+    { id: 1, title: 'Часы', count: 10, price: 500, producer: 'Германия' },
+    { id: 2, title: 'Смартфон', count: 33, price: 1500 , producer: 'Германия' },
+    { id: 3, title: 'Моноблок', count: 6, price: 2200, producer: 'Германия' },
+    { id: 4, title: 'Ноутбук', count: 13, price: 3000, producer: 'Китай' },
+    { id: 5, title: 'Планшет', count: 22, price: 2100, producer: 'Китай' },
+    { id: 6, title: 'Телевизор', count: 4, price: 4100, producer: 'Германия' },
+    { id: 7, title: 'Холодильник', count: 11, price: 2400, producer: 'Германия' }
+    ]
+describe("набор тестов 4", () => {
+    test("Succes", () => {
+      const result = findProduct(arr);
+      expect(result).toEqual(
+        [
+            { id: 2, title: 'Смартфон', count: 33, price: 1500 , producer: 'Германия' },
+            { id: 7, title: 'Холодильник', count: 11, price: 2400, producer: 'Германия' }
+        ]
+      );
+    })
+    test("Success", ()=>{
+      const result = findProduct('string');
+      expect(result).toBe('not array');
+    })
+    test("Success", ()=>{
+      const result = findProduct([]);
+      expect(result).toBe('empty array');
+    })
+    test("Success", ()=>{
+      const result = findProduct(arr);
+      expect(result).toHaveLength(2);
+    })
+})
+const arrayOfPhoneNumbers = [
+  "+375331111111",
+  "+375331111111",
+  "+375441234567",
+  "+375441234567",
+  "+375297756871",
+];
+describe('набор тестов 5', ()=>{
+  test("Success", ()=>{
+    const result = uniqNumbers('stringNotArray');
+    expect(result).toBe('not an array')
+  })
+  test("Success", ()=>{
+    const result = uniqNumbers([]);
+    expect(result).toBe('Empty array')
+  })
+  test("Success", ()=>{
+    const result = uniqNumbers([
+      "+375297756871",
+      123
+    ]);
+    expect(result).toBe('не все элементы - строки')
+  })
+  test("Success", ()=>{
+    const result = uniqNumbers([
+      "+375297756871",
+      "+37529775"
+    ]);
+    expect(result).toBe('не все элементы - phone numbers')
+  })
+  test("Success", ()=>{
+    const result = uniqNumbers(arrayOfPhoneNumbers);
+    expect(result).toEqual([
+      "+375331111111",
+      "+375441234567",
+      "+375297756871",
+    ]);
+  })
+})
