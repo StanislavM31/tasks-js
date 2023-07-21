20d8a885594a638c1d2fafa287e5334f48c02cbc
Stanislav Minko <minkostas@gmail.com>
2023-07-21 22:40:29 +0300

    jest.2: 5-11

--- 070.unitTestsTypeScript/app.test.ts
+++ 070.unitTestsTypeScript/app.test.ts
@@ -1,75 +1,150 @@
-import {isPalindrome, calculateFactorial, capitalizeString, flattenArray,deleteDublicate} from './app';
+import {
+    isPalindrome, calculateFactorial,
+    capitalizeString, flattenArray,
+    deleteDublicate, findMissingNumber,
+    findPairWithSum, StringArray,
+    NumberArray, PersonArray
+} from './app';
 
-describe('TestSuite_1', ()=>{
-    test('Success', ()=>{
+describe('TestSuite_1', () => {
+    test('Success', () => {
         const result = isPalindrome('NonPalyndromstring');
         expect(result).toBe(false)
     })
-    test('Success', ()=>{
+    test('Success', () => {
         const result = isPalindrome('weew');
         expect(result).toBe(true);
     })
-    test('Success', ()=>{
+    test('Success', () => {
         const result = isPalindrome(123);
         expect(result).toBe(`not a string`);
     })
 })
 
-describe('TestSuite_2', ()=>{
-    test('Success', ()=>{
+describe('TestSuite_2', () => {
+    test('Success', () => {
         const result = calculateFactorial('factorial');
         expect(result).toBe('not a number');
     })
-    test('Success', ()=>{
+    test('Success', () => {
         const result = calculateFactorial(-10);
         expect(result).toBe('нельзя найти факториал числа <0');
     })
-    test('Success', ()=>{
+    test('Success', () => {
         const result = calculateFactorial(5);
         expect(result).toBe(120);
     })
 })
-describe('TestSuite_3', ()=>{
-    test('Success', ()=>{
+describe('TestSuite_3', () => {
+    test('Success', () => {
         const result = capitalizeString(123);
         expect(result).toBe('передана не строка');
     })
-    test('Success', ()=>{
+    test('Success', () => {
         const result = capitalizeString("привет студент школы hs");
         expect(result).toBe('Привет Студент Школы Hs');
     })
 
 
 })
-describe('TestSuite_4', ()=>{
-    test('Success', ()=>{
-        const result = flattenArray([1,2,[3]]);
-        expect(result).toEqual([1,2,3]);
+describe('TestSuite_4', () => {
+    test('Success', () => {
+        const result = flattenArray([1, 2, [3]]);
+        expect(result).toEqual([1, 2, 3]);
     })
-    test('Success', ()=>{
-        const result = flattenArray([1,2,[3,4,5,[6,[7,8]]],]);
-        expect(result).toEqual([1,2,3,4,5,6,7,8]);
+    test('Success', () => {
+        const result = flattenArray([1, 2, [3, 4, 5, [6, [7, 8]]],]);
+        expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
     })
-    test('Success', ()=>{
+    test('Success', () => {
         const result = flattenArray("string");
         expect(result).toBe('not array');
     })
-    test('Success', ()=>{
+    test('Success', () => {
         const result = flattenArray([]);
         expect(result).toBe('empty array');
     })
-    test('Success', ()=>{
-        const result = flattenArray([1,2,3,4,5,6,7,8,9,0]);
-        expect(result).toEqual([1,2,3,4,5,6,7,8,9,0]);
+    test('Success', () => {
+        const result = flattenArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+        expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
     })
 
 })
-
-describe('TestSuite_10', ()=>{
-    test('Success', ()=>{
+describe('TestSuite_6', () => {
+    test('Success', () => {
+        const result = findMissingNumber([1, 2, 3, 5, 6, 7, 8, 9])
+        expect(result).toBe(4);
+    })
+})
+describe('TestSuite_7', () => {
+    test('Success', () => {
+        const result = findPairWithSum([1, 5, 2, 3, 5, 7], 10)
+        expect(result).toEqual([5, 5]);
+    })
+    test('Success', () => {
+        const result = findPairWithSum([1, 10, 9, 5, 1, 6], 15)
+        expect(result).toEqual([10, 5]);
+    })
+    test('Success', () => {
+        const result = findPairWithSum([1, 1, 1, 1, 1, 1], 3)
+        expect(result).toEqual(null);
+    })
+})
+describe('TestSuite_8', () => {
+    let stringArray = new StringArray();
+    test('Success', () => {
+        const longest = stringArray.getLongestWord()
+        expect(longest).toBe('Ezequil');
+    })
+    test('Succes', () => {
+        const unique = stringArray.getUniqueWords()
+        expect(unique).toEqual(['Hello', 'my', 'dear', 'Ezequil']);
+    })
+})
+describe('TestSuite_9', () => {
+    let numberArray = new NumberArray();
+    test('Succes', () => {
+        const sum = numberArray.getSum();
+        expect(sum).toBe(152);
+    })
+    test('Succes', () => {
+        const even = numberArray.getEvenNumbers();
+        expect(even).toEqual([10, 20, 30]);
+    })
+})
+describe('TestSuite_10', () => {
+    test('Success', () => {
+        let personArray = new PersonArray();
+        const names = personArray.getNames();
+        expect(names).toEqual([
+            'Anna',
+            'Bill',
+            'Coul',
+            'Gregory',
+            'Sam']);
+    })
+    let personArray = new PersonArray();
+    const adults = personArray.getAdults();
+    expect(adults).toEqual([
+        { name: 'Bill', age: 20 },
+        { name: 'Coul', age: 40 },
+        { name: 'Gregory', age: 50 },
+        { name: 'Sam', age: 30 },
+    ]);
+    test('Success', () => {
+        const average = personArray.getAverageAge();
+        expect(average).toBe(30);
+    })
+})
+describe('TestSuite_11', () => {
+    test('Success', () => {
         const result = deleteDublicate('AAAABBBCCDAABBB');
-        console.log(result);
-
-        /* expect(result).toBe('ABCDAB') */
+        expect(result).toBe('ABCDAB');
+    })
+})
+describe('TestSuite_12', () => {
+    test('Success', () => {
+        const result = deleteDublicate('AAAABBBCCDAABBB');
+        expect(result).toBe('ABCDAB');
     })
 })
--- 070.unitTestsTypeScript/app.ts
+++ 070.unitTestsTypeScript/app.ts
@@ -4,11 +4,11 @@
 направо и справа налево), и false в противном случае
 */
 
-function isPalindrome(word: any): boolean{
+function isPalindrome(word: any): boolean {
     try {
-        if(typeof word !== 'string') throw new Error(`not a string`);
+        if (typeof word !== 'string') throw new Error(`not a string`);
         const reverseWord = word.split('').reverse().join('');
-        if(reverseWord !== word) return false;
+        if (reverseWord !== word) return false;
         return true
     } catch (error) {
         return error.message;
@@ -19,14 +19,14 @@ function isPalindrome(word: any): boolean{
 2. Напишите функцию calculateFactorial(n: number): number, которая принимает
 число n и возвращает его факториал.
 */
-function calculateFactorial(n: any): number|string{
+function calculateFactorial(n: any): number | string {
     try {
-        if(typeof n !== 'number') throw new Error("not a number");
-        if(n<0) throw new Error("нельзя найти факториал числа <0");
-        let result:number = 1;
-        for (let i:number = 1; i <= n; i++) {
+        if (typeof n !== 'number') throw new Error("not a number");
+        if (n < 0) throw new Error("нельзя найти факториал числа <0");
+        let result: number = 1;
+        for (let i: number = 1; i <= n; i++) {
             const element = i;
-            result*=element;
+            result *= element;
         }
         return result
     } catch (error) {
@@ -38,36 +38,160 @@ function calculateFactorial(n: any): number|string{
 возвращает новую строку, в которой каждое слово начинается с заглавной буквы.
 */
 
-function capitalizeString(str: any): string{
-  try {
-    if(typeof str !== 'string') throw new Error('передана не строка');
-    let temp:string[] = str.split(' ');
-    let result = '';
-    for (let i = 0; i < temp.length; i++) {
-        const element = temp[i];
-        result += element[0].toUpperCase()+element.slice(1)+' ';
-    }
-    return result.trim();
-  } catch (error) {
-    return error.message;
-  }
-}
-
-/* 4. Напишите функцию flattenArray(array: any[]): any[], которая принимает массив, в
-котором могут быть вложенные массивы, и возвращает новый массив, в котором
-все элементы являются плоским списком без вложенности. */
-function flattenArray(array: any): any[]{
+function capitalizeString(str: any): string {
     try {
-        if(!Array.isArray(array)) throw new Error('not array')
-        if(!array.length) throw new Error('empty array')
-        return array.flat(Infinity);
+        if (typeof str !== 'string') throw new Error('передана не строка');
+        let temp: string[] = str.split(' ');
+        let result = '';
+        for (let i = 0; i < temp.length; i++) {
+            const element = temp[i];
+            result += element[0].toUpperCase() + element.slice(1) + ' ';
+        }
+        return result.trim();
     } catch (error) {
         return error.message;
     }
 }
 
+/* 4. Напишите функцию flattenArray(array: any[]): any[], которая принимает массив, в
+котором могут быть вложенные массивы, и возвращает новый массив, в котором
+все элементы являются плоским списком без вложенности. */
+function flattenArray(array: any): any[] {
+    try {
+        if (!Array.isArray(array)) throw new Error('not array')
+        if (!array.length) throw new Error('empty array')
+        return array.flat(Infinity);
+    } catch (error) {
+        return error.message;
+    }
+}
 /*
-10. Реализуйте функцию, которая принимает в качестве параметра строку и
+6. Напишите функцию findMissingNumber(numbers: number[]): number, которая
+принимает массив чисел, в котором пропущено одно число из
+последовательности, и возвращает пропущенное число.
+*/
+function findMissingNumber(numbers: number[]): number {
+    let result: number = numbers[0];
+    for (let i: number = 0; i < numbers.length; i++) {
+        if (numbers[i + 1]) {
+            if (numbers[i + 1] - numbers[i] !== 1) {
+                result = numbers[i] + 1;
+            }
+        }
+    }
+    return result;
+}
+/*
+7. Напишите функцию findPairWithSum(numbers: number[], targetSum: number):
+[number, number] | null, которая принимает массив чисел и целевую сумму, и
+возвращает пару чисел из массива, сумма которых равна целевой сумме. Если
+такая пара не найдена, функция должна возвращать null.
+ */
+function findPairWithSum(numbers: number[], targetSum: number): [number, number] | null {
+    let result: [number, number] = [0, 0];
+    for (let i: number = 0; i < numbers.length; i++) {
+        for (let j: number = 0; j < numbers.length; j++) {
+            if (i !== j) {
+                if (numbers[i] + numbers[j] == targetSum) {
+                    result[0] = (numbers[i]);
+                    result[1] = (numbers[j]);
+                    return result;
+                }
+            }
+        }
+    }
+    return null;
+}
+/*
+8. Создайте класс StringArray, который имеет свойство array (массив строк) и методы:
+getLongestWord(): string - возвращает самое длинное слово из массива.
+getUniqueWords(): string[] - возвращает массив уникальных слов из массива.
+*/
+class StringArray {
+    array: string[] = ['Hello', 'my', 'dear', 'Ezequil', 'Hello', 'my', 'dear'];
+    getLongestWord(): string {
+        let l: number = 0;
+        let result: string = '';
+        this.array.forEach((el: string) => {
+            if (l < el.length) {
+                l = el.length;
+                result = el
+            };
+        })
+        return result
+    };
+    getUniqueWords(): string[] {
+        let uniqueString: string[] = [];
+        this.array.forEach((el: string) => {
+            if (!uniqueString.includes(el)) {
+                uniqueString.push(el)
+            }
+        })
+        return uniqueString;
+    };
+}
+/*
+9. Создайте класс NumberArray, который имеет свойство array (массив чисел) и
+методы:
+getSum(): number - возвращает сумму всех чисел в массиве.
+getEvenNumbers(): number[] - возвращает массив только четных чисел из массива.
+*/
+class NumberArray {
+    array: number[] = [10, 20, 30, 41, 51]
+    getSum(): number {
+        return this.array.reduce((acc: number, next: number) => acc + next, 0);
+    }
+    getEvenNumbers(): number[] {
+        let filtered: number[] = []
+        filtered = this.array.filter((el: number) => el % 2 == 0);
+        return filtered;
+    }
+}
+/*
+10. Создайте класс PersonArray, который имеет свойство array (массив объектов
+Person) и методы:
+getNames(): string[] - возвращает массив имен всех людей.
+getAdults(): Person[] - возвращает массив только совершеннолетних людей.
+getAverageAge(): number - возвращает средний возраст всех людей.
+*/
+interface Person {
+    name: string;
+    age: number;
+}
+class PersonArray {
+    array: Person[] = [
+        { name: 'Anna', age: 10 },
+        { name: 'Bill', age: 20 },
+        { name: 'Coul', age: 40 },
+        { name: 'Gregory', age: 50 },
+        { name: 'Sam', age: 30 },
+    ]
+    getNames(): string[]{
+        const names:string[]=[];
+        this.array.forEach(element => {
+            names.push(element.name);
+        });
+        return names;
+    }
+    getAdults(): Person[]{
+        const adults:Person[]=[];
+        this.array.forEach(element => {
+            if(element.age>18){
+                adults.push(element);
+            }
+        });
+        return adults;
+    }
+    getAverageAge(): number{
+        let allAge:number = 0;
+        this.array.map((el:Person)=>{
+            allAge+=el.age
+        })
+        return allAge/this.array.length;
+    }
+}
+/*
+12. Реализуйте функцию, которая принимает в качестве параметра строку и
 возвращает массив без каких-либо элементов с одинаковым значением рядом
 друг с другом.
 Написать тест для функции
@@ -76,22 +200,26 @@ function flattenArray(array: any): any[]{
 '12233’ -> [1, 2, 3]
 Написать тест для функции
 */
-
-function deleteDublicate(string){
+function deleteDublicate(string:string) {
     let result = '';
-    for (let i = 0; i < string.length; i++) {
-        let element = string[i];
-        if(element == string[i+1] || element == string[i-1]) {
-            if(result[result.length-1] == element){
+    for (let i:number = 0; i < string.length; i++) {
+        let element:string = string[i];
+        if (element == string[i + 1] || element == string[i - 1]) {
+            if (result[result.length - 1] == element) {
                 continue;
             } else {
-                result+=element
+                result += element
             }
         }
         else {
-            result+= element;
+            result += element;
         }
     }
     return result;
 }
-export {isPalindrome, calculateFactorial, capitalizeString, flattenArray, deleteDublicate}
+export {
+    isPalindrome, calculateFactorial,capitalizeString,
+    flattenArray, deleteDublicate, findMissingNumber,
+    findPairWithSum,
+    StringArray, NumberArray, PersonArray
+}
