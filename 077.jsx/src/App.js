648d808e3ef13738bbe1166efacfa0a4b7b7e3f3
Stanislav Minko <minkostas@gmail.com>
2023-08-16 02:31:43 +0300

    077.jsx_2: 7(2task)

--- 077.jsx/src/App.js
+++ 077.jsx/src/App.js
@@ -4,6 +4,7 @@ import Task3 from "./pages/Task3"
 import Task4 from "./pages/Task4"
 import Task5 from "./pages/Task5"
 import Task6 from "./pages/Task6"
+import Task7 from "./pages/Task7"
 import Task8 from "./pages/Task8"
 import Task9 from "./pages/Task9"
 import Task10 from "./pages/Task10"
@@ -24,6 +25,8 @@ export default function App(){
       <Task5/>
       <hr />
       <Task6/>
+      <hr />
+      <Task7/>
       <hr />
       <Task8/>
       <hr />
---
+++ 077.jsx/src/pages/Task7.js
@@ -0,0 +1,26 @@
+/*
+2. По клику на кнопку собрать данные из input и проверить вводимую строку на
+палиндром
+*/
+import { useState } from "react";
+
+export default function Task7(){
+    const[inp, setInput] = useState("");
+    function setNewValueInp(e){
+        setInput(e.target.value);
+    }
+    function isPolyndrom(){
+        if(inp===inp.split("").reverse().join("")){
+            alert('POLYNDROM')
+        } else {
+            alert('NOT POLYNDROM')
+        }
+    }
+    return(
+        <>
+        <p>Task#7</p>
+        <input type="text" placeholder="->..." onChange={setNewValueInp}/>
+        <button onClick={isPolyndrom}>check</button>
+        </>
+    )
+}
