525ad24a573ab371986227a76d289e59ded97c43
Stanislav Minko <minkostas@gmail.com>
2023-09-21 05:39:14 +0300

    :useReducer_1

--- 078_react-hooks/src/pages/useReducer/Task1.js
+++ 078_react-hooks/src/pages/useReducer/Task1.js
@@ -1,29 +1,28 @@
-/*
-useReducer:
-1. Создайте компонент счетчика, который может увеличивать или уменьшать
-значение при нажатии на соответствующие кнопки.
-*/
 
-import {useCallback, useState, useReducer} from 'react';
+import { useReducer} from "react";
 
-function reducer(state, action){
+function reducer(count, action){
     switch(action){
-        case 'INCREMENT': return state+1;
-        case 'DECREMENT': return state-1;
-        default: break;
+        case "Increment":
+            return count+1
+        case "Decrement":
+            return count-1
+            default: break;
     }
 }
 
-export default function Task1(){
-    const [ value, dispatch] = useReducer(reducer, 0);
+export default function Task1() {
+    const [count, dispatch] = useReducer(reducer, 0);
 
-
-    return(
-        <>
-        <h1>{value}</h1>
-        <button onClick={()=> dispatch("INCREMENT")}>+</button>
-        <button onClick={()=> dispatch("DECREMENT")}>+</button>        </>
-    )
-
+  return (
+    <>
+      <p>
+        1. Создайте компонент счетчика, который может увеличивать или уменьшать
+        значение при нажатии на соответствующие кнопки.
+      </p>
+      <h1>{count}</h1>
+      <button onClick={()=>{ dispatch("Increment")}}>Plus</button>
+      <button onClick={()=>{ dispatch("Decrement")}}>Minus</button>
+    </>
+  );
 }
-
