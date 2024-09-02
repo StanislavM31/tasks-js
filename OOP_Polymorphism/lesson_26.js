a5f2a2e9ec38a3c882a1318bcfba51d6fa2ef281
Stanislav Minko <60983985+StanislavM31@users.noreply.github.com>
2024-09-02 19:46:37 +0300

    OOP_Polymorth_lesson_26.js

--- OOP_Polymorphism/lesson_26.js
+++ OOP_Polymorphism/lesson_26.js
@@ -180,3 +180,24 @@ class Number {
 }
 let number = new Number(12321);
 number.PalindromeChecker();
+/*6. Создайте родительский класс Number, который будет хранить статичное число. Затем создайте
+дочерний класс SquareRootCalculator, который будет содержать метод calculateSquareRoot(),
+вычисляющий квадратный корень этого числа без использования встроенной функции Math.sqrt().
+Входные: new SquareRootCalculator(16) → Результат: 4
+Входные: new SquareRootCalculator(25) → Результат: 5
+Входные: new SquareRootCalculator(1) → Результат: 1
+*/
+class Number2 {
+  constructor(num){
+    this.num = num;
+  }
+  squareRoot () {
+  for (let i = 0; i <= this.num; i++) {
+    if (i * i === this.num) return i;
+  }
+  
+};
+}
+
+let number2 = new Number2(25);
+console.log(number2.squareRoot());
