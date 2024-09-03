57d4ed659a511b93ec180e5e1c43d985fc20238a
Stanislav M <minkostas@gmail.com>
2024-09-03 23:59:20 +0300

    :_7

--- OOP_Polymorphism/lesson_26.js
+++ OOP_Polymorphism/lesson_26.js
@@ -201,3 +201,28 @@ class Number2 {
 
 let number2 = new Number2(25);
 console.log(number2.squareRoot());
+/* 7. Создайте родительский класс NumberGenerator, который будет генерировать и возвращать
+случайное число с помощью метода generateNumber(). Затем создайте дочерний класс
+SquareRootCalculator, который переопределяет метод generateNumber() и находит квадратный
+корень этого числа без использования встроенной функции Math.sqrt().
+Входные: new SquareRootCalculator().generateNumber() (предположим, сгенерированное число 16)→
+Результат: 4
+Входные: new SquareRootCalculator().generateNumber() (предположим, сгенерированное число 25) →
+Результат: 5 */
+
+class NumberGenerator{
+  generateNumber(){
+    let n = Math.random()*10;
+    console.log("thi is generateNumber method");
+    return n;
+  }
+}
+
+class SquareRootCalculator extends NumberGenerator{
+   generateNumber(){
+    let num = super.generateNumber();
+
+  }
+}
+
+let n = new SquareRootCalculator.generateNumber();
