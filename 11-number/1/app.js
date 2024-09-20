da403175ecec9d812fd1c9a6acfba5aa4c0d8f6c
Stanislav Minko <minkostas@gmail.com>
2024-09-20 18:45:02 +0300

    :{d: null}

--- 11-number/1/app.js
+++ 11-number/1/app.js
@@ -1,4 +1,65 @@
-/* Даны переменные a = 10 и b = 3. Найдите остаток от деления a на b. */
-let a = 10, b = 3;
+/* {
+  a: {
+    b: {
+      c: {
+        d: null;
+      }
+    }
+  }
+} */
 
-console.log(a%b);
+const string = "a.b.c.d";
+
+function foo() {
+  let string = str.split(".");
+  console.log(string);
+  let object = {};
+  let l = string.length-1;
+
+  let temp = {}
+  
+  for (let i = l; i >= 0; i--) {
+    const element = string[i];
+    console.log("element: ", element);
+    console.log(object);
+    
+
+if(i==l){
+    object[element]=null;
+    temp = {object};
+} else {
+
+    object[element]=temp; //object.c = d: null
+    temp = {object};
+}
+
+  }
+}
+
+foo();
+console.log("object", object.a);
+console.log("object", object.b);
+console.log("object", object.c);
+console.log("object", object.d);
+/* const str='a.b.c.d'
+{
+}
+{
+a:{
+b:{
+c:{
+d:null
+}
+}
+}
+} */
+
+
+const str='a.b.c.d';
+
+
+const result=str.split('.').reduceRight((acc,item)=>{
+    return {[item]:acc}
+},null)
+
+console.log(result)
